fd4263d5725d2b78d5c0b6e01a3ca660710cd330
HariomYadav99 <134788017+HariomYadav99@users.noreply.github.com>
2023-12-09 13:08:12 +0000

    done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,35 @@
+"use strict"; // Treat all JS code as newer version.
+
+// alert("HEllo")
+// alert(3 + 3) // We are using node.js not browser.
+
+
+// Code readability should be high.
+console.log(3 +3);
+
+console.log("Hariom")
+
+let name = "Hariom"
+let age = 13
+let isLoggedIn = false
+let state = null
+
+// PRIMITIVE DATA TYPES-->
+
+// number ==> 2 to power 53
+// bigInt
+// string ==> ""
+// boolean ==> true/false
+// null ==> standalone value
+// undefined ==>
+// symbol ==> for unique
+
+// NON PRIMITIVE DATA TYPES -->
+
+//object
+
+console.log(typeof "Hariom")
+console.log(typeof age);
+console.log(typeof null); // object.
+console.log(typeof undefined);//undefined.
+
