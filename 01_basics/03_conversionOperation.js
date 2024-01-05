f50a014c2f37f8b95a668d5167e5d4fe5342d685
HariomYadav99 <134788017+HariomYadav99@users.noreply.github.com>
2024-01-05 12:02:49 +0000

    update

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -68,26 +68,26 @@ let value = 3;
 let negValue = -value;
 console.log(negValue);
 
-console.log(2+2);
-console.log(2-2);
-console.log(2*2);
-console.log(2**3);
-console.log(2/3);
-console.log(2%3);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
 
 let str1 = "Hello";
 let str2 = " Hariom";
 let str3 = str1 +str2;
 console.log(str3);
 
-console.log("1" + 2);
-console.log(1 +"2");
-console.log("1" + 2 +2);
-console.log(1 + 2 + "2");
-console.log(((3 + 4) * 5) % 3);
-console.log(true);
-console.log(+true);
-console.log(+"");
+// console.log("1" + 2);
+// console.log(1 +"2");
+// console.log("1" + 2 +2);
+// console.log(1 + 2 + "2");
+// console.log(((3 + 4) * 5) % 3);
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
 
 let num1, num2, num3;
 num1 = num2 = num3 = 2 + 2
