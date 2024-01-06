c44ebd6c5fe8e24328374b812239b9f3e47ccfd0
HariomYadav99 <134788017+HariomYadav99@users.noreply.github.com>
2024-01-06 14:09:29 +0000

    update

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -68,4 +68,44 @@ function loginUserMessage(username ="Harry"){
 
 // console.log(loginUserMessage("Hariom"))
 // console.log(loginUserMessage(""))
-console.log(loginUserMessage())
+//console.log(loginUserMessage())
+
+
+// rest operator(...) or spread operator
+function calculateCartPrice(...num1){ // ...==> rest operator
+    return num1
+}
+
+console.log(calculateCartPrice(200,400,500))
+
+
+
+// function calculateCartPrice(val1, val2, ...num1){
+//     return num1
+// }
+
+// //console.log(calculateCartPrice(200,400,500,2000))
+
+const user ={
+    userName: "hariom",
+    price : 199    
+}
+
+function handObject(anyobject){
+    console.log(`Username is ${anyobject.userName} and price is ${anyobject.price}`)
+}
+
+// handObject(user)
+handObject({
+    userName: "Harry",
+    price: 499
+})
+
+const myNewArray = [200, 400, 500, 600]
+
+function returnSecondValue(getArray){
+    return getArray[2]
+}
+
+// console.log(returnSecondValue(myNewArray))
+console.log(returnSecondValue([200, 400, 500, 600]))
