27d3e2b075af6bc27e2d2abb8adb72eff19f8e3a
HariomYadav99 <134788017+HariomYadav99@users.noreply.github.com>
2024-01-07 19:03:14 +0000

    scopeUpdate

---
+++ 03_basics/03_scopes.js
@@ -0,0 +1,18 @@
+let a = 300
+// const b = 20
+ var c = 3000
+
+
+{}
+
+
+if(true){
+    let a = 10
+    const b = 20
+    var c = 30
+    console.log("INNER: " ,a)
+}
+
+console.log(a);
+// console.log(b);
+console.log(c);
