47a3386aa75c12c6a68a3a533e3380dbb794fe71
HariomYadav99 <134788017+HariomYadav99@users.noreply.github.com>
2024-01-08 12:31:41 +0000

    switchUpdated

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,52 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3
+switch (month) {
+    case 1 :
+        console.log("jan")
+        break;
+    case 2 :
+        console.log("Febuary")
+        break;
+    case 3 :
+        console.log("March")
+        break;
+    case 4 :
+        console.log("April")
+        break;
+    case 5 :
+        console.log("june")
+        break;
+    case 6 :
+        console.log("july")
+        break;
+    case 7 :
+        console.log("August")
+        break;
+    case 8 :
+        console.log("september")
+        break;
+    case 9 :
+        console.log("October")
+        break;
+    case 10 :
+        console.log("November")
+        break;
+    case 11 :
+        console.log("December")
+        break;
+    case 12 :
+        console.log("jan")
+        break;
+
+    default:
+        console.log("default case match")
+        break;
+}
