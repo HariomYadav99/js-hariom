28a94b3b020c1b1b1733077d9a56a0c992572c48
HariomYadav99 <134788017+HariomYadav99@users.noreply.github.com>
2024-01-08 12:22:00 +0000

    Updated

---
+++ 04_control_flow/one.js
@@ -0,0 +1,76 @@
+// if 
+
+const isUsersLoggedIn = true
+const temperature = 41
+
+// if(temperature< 50){
+//     console.log("Less than 50;");
+// }else{
+// console.log("tempearature is greater than 50")
+// }
+
+// console.log("Executed")
+
+// if(isUsersLoggedIn){
+
+// }
+
+// < , > , <= ,>=,  ==, != , === , !==
+
+
+// if(2== "2"){
+//     console.log("Executed")
+// }
+
+// if(2==="2"){
+//     console.log("Executed")
+// }
+
+if(true){
+    
+}
+
+if (2==2){
+
+}
+
+if(false){
+    
+}
+
+// const score = 200
+// if(score > 100){
+//     const power = "Fly"
+//     // var power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+
+// // console.log(`User power: ${power}`);
+
+// const balance = 1000
+// // if(balance > 500) console.log("Test"), console.log('test2');
+
+// if(balance < 500 ){
+//     console.log("less than")
+// }else if(balance < 750){
+//     console.log("less than 750")
+// }else if (balance < 900){
+//     console.log("less than 900")
+// }else{
+//     console.log("less than 1200")
+// }
+
+
+const userLoggedIn = true
+const debitcard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+const guestUser = false
+
+if(userLoggedIn && debitcard && 2 == 2 ){
+    console.log("Allow to buy course.")
+}
+
+if(loggedInFromEmail || loggedInFromGoogle || guestUser){
+    console.log("User logged In")
+}
